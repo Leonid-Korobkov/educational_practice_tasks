5bef4bff79b5bd500917fc9ef4eea814a5aa4b64
krb_ln <krb_ln@DESKTOP-QUEQK47>
2024-02-18 20:24:34 +0300

    Add project files.

---
+++ educational_practice_tasks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace educational_practice_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
