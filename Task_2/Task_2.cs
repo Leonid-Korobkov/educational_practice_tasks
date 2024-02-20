530a2b08c46ae60ebfb94a752030c411068d34e1
леонид коробков <korobkov-leo@mail.ru>
2024-02-20 09:13:16 +0300

    tasks 1, 2 have been completed

---
+++ Task_2/Task_2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Task_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты слона x1y1 и координаты фигуры x2y2:");
+            string input = Console.ReadLine();
+
+            if (input.Length == 0)
+            {
+                Console.WriteLine("Вы ввели некорректные координаты");
+                ExitTheProgram();
+                return;
+            }
+
+            string[] coordinates = input.Split(' ');
+
+            if (coordinates.Length != 2)
+            {
+                Console.WriteLine("Вы ввели некорректные координаты");
+                ExitTheProgram();
+                return;
+            }
+
+            char x1 = coordinates[0][0];
+            char y1 = coordinates[0][1];
+            char x2 = coordinates[1][0];
+            char y2 = coordinates[1][1];
+
+            // Проверяем корректность введенных координат
+            if (!IsValidCoordinate(x1, y1) || !IsValidCoordinate(x2, y2))
+            {
+                Console.WriteLine("Вы ввели некорректные координаты");
+                ExitTheProgram();
+                return;
+            }
+
+            // Проверяем, бьет ли слон фигуру
+            if (Math.Abs(x1 - x2) == Math.Abs(y1 - y2))
+            {
+                Console.WriteLine("Слон сможет побить фигуру");
+            }
+            else
+            {
+                Console.WriteLine("Слон не сможет побить фигуру");
+            }
+            ExitTheProgram();
+        }
+
+        static bool IsValidCoordinate(char x, char y)
+        {
+            return x >= 'a' && x <= 'h' && y >= '1' && y <= '8';
+        }
+
+        static void ExitTheProgram()
+        {
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
