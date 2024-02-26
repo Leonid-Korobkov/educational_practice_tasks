1a737145eb74ae75958995930828356c7063c7a0
леонид коробков <korobkov-leo@mail.ru>
2024-02-26 16:07:19 +0300

    tasks 11 and 12 have been completed + add readme

---
+++ Task_11-12/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Task_11_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnShowSolution = new System.Windows.Forms.Button();
+            this.BtnUnlockCells = new System.Windows.Forms.Button();
+            this.BtnNewGame = new System.Windows.Forms.Button();
+            this.BtnLoadSave = new System.Windows.Forms.Button();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.BtnConditionImposed = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnShowSolution
+            // 
+            this.BtnShowSolution.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BtnShowSolution.Location = new System.Drawing.Point(855, 91);
+            this.BtnShowSolution.Margin = new System.Windows.Forms.Padding(10);
+            this.BtnShowSolution.Name = "BtnShowSolution";
+            this.BtnShowSolution.Size = new System.Drawing.Size(189, 81);
+            this.BtnShowSolution.TabIndex = 0;
+            this.BtnShowSolution.Text = "Показать решение";
+            this.BtnShowSolution.UseVisualStyleBackColor = false;
+            this.BtnShowSolution.Click += new System.EventHandler(this.BtnShowSolution_Click);
+            // 
+            // BtnUnlockCells
+            // 
+            this.BtnUnlockCells.Location = new System.Drawing.Point(855, 394);
+            this.BtnUnlockCells.Margin = new System.Windows.Forms.Padding(10);
+            this.BtnUnlockCells.Name = "BtnUnlockCells";
+            this.BtnUnlockCells.Size = new System.Drawing.Size(189, 81);
+            this.BtnUnlockCells.TabIndex = 9;
+            this.BtnUnlockCells.Text = "Разблокировать ячейки";
+            this.BtnUnlockCells.UseVisualStyleBackColor = true;
+            this.BtnUnlockCells.Click += new System.EventHandler(this.BtnUnlockCells_Click);
+            // 
+            // BtnNewGame
+            // 
+            this.BtnNewGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.BtnNewGame.Cursor = System.Windows.Forms.Cursors.Default;
+            this.BtnNewGame.Location = new System.Drawing.Point(855, 192);
+            this.BtnNewGame.Margin = new System.Windows.Forms.Padding(10);
+            this.BtnNewGame.Name = "BtnNewGame";
+            this.BtnNewGame.Size = new System.Drawing.Size(189, 81);
+            this.BtnNewGame.TabIndex = 8;
+            this.BtnNewGame.Text = "Новая игра";
+            this.BtnNewGame.UseVisualStyleBackColor = false;
+            this.BtnNewGame.Click += new System.EventHandler(this.BtnNewGame_Click);
+            // 
+            // BtnLoadSave
+            // 
+            this.BtnLoadSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.BtnLoadSave.Cursor = System.Windows.Forms.Cursors.Default;
+            this.BtnLoadSave.Location = new System.Drawing.Point(855, 596);
+            this.BtnLoadSave.Margin = new System.Windows.Forms.Padding(10);
+            this.BtnLoadSave.Name = "BtnLoadSave";
+            this.BtnLoadSave.Size = new System.Drawing.Size(189, 99);
+            this.BtnLoadSave.TabIndex = 7;
+            this.BtnLoadSave.Text = "Загрузить сохраненное решение";
+            this.BtnLoadSave.UseVisualStyleBackColor = false;
+            this.BtnLoadSave.Click += new System.EventHandler(this.BtnLoadSave_Click);
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.BtnSave.Location = new System.Drawing.Point(855, 495);
+            this.BtnSave.Margin = new System.Windows.Forms.Padding(10);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(189, 81);
+            this.BtnSave.TabIndex = 6;
+            this.BtnSave.Text = "Сохранить решение";
+            this.BtnSave.UseVisualStyleBackColor = false;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // BtnConditionImposed
+            // 
+            this.BtnConditionImposed.Location = new System.Drawing.Point(855, 293);
+            this.BtnConditionImposed.Margin = new System.Windows.Forms.Padding(10);
+            this.BtnConditionImposed.Name = "BtnConditionImposed";
+            this.BtnConditionImposed.Size = new System.Drawing.Size(189, 81);
+            this.BtnConditionImposed.TabIndex = 5;
+            this.BtnConditionImposed.Text = "Условие введено";
+            this.BtnConditionImposed.UseVisualStyleBackColor = true;
+            this.BtnConditionImposed.Click += new System.EventHandler(this.BtnConditionImposed_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 860);
+            this.Controls.Add(this.BtnUnlockCells);
+            this.Controls.Add(this.BtnNewGame);
+            this.Controls.Add(this.BtnLoadSave);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.BtnConditionImposed);
+            this.Controls.Add(this.BtnShowSolution);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Игра \"Судоку\"";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnShowSolution;
+        private System.Windows.Forms.Button BtnUnlockCells;
+        private System.Windows.Forms.Button BtnNewGame;
+        private System.Windows.Forms.Button BtnLoadSave;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnConditionImposed;
+    }
+}
+
---
+++ Task_11-12/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Task_11_12
+{
+    public partial class Form1 : Form
+    {
+        private TextBox[,] sudokuCells; // Массив для хранения ячеек судоку
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSudokuGrid(); // Инициализация сетки судоку
+        }
+
+        // Метод для инициализации сетки судоку
+        private void InitializeSudokuGrid()
+        {
+            sudokuCells = new TextBox[9, 9]; // Создание двумерного массива для ячеек судоку
+            const int cellSize = 40; // Размер ячейки
+
+            // Создание и расположение текстовых полей для ячеек судоку
+            for (int i = 0; i < sudokuCells.GetLength(0); i++)
+            {
+                for (int j = 0; j < sudokuCells.GetLength(1); j++)
+                {
+                    int squareRow = i / 3;
+                    int squareCol = j / 3;
+                    Color backgroundColor = (squareRow + squareCol) % 2 == 0 ? Color.Cyan : Color.White; // Определение цвета фона ячейки
+
+                    var cell = new TextBox // Создание новой ячейки судоку
+                    {
+                        Location = new Point(j * cellSize + 20, i * cellSize + 20), // Установка положения ячейки на форме
+                        Size = new Size(cellSize, cellSize), // Установка размера ячейки
+                        Font = new Font(FontFamily.GenericSansSerif, 16), // Установка шрифта текста в ячейке
+                        TextAlign = HorizontalAlignment.Center, // Установка выравнивания текста по центру
+                        Multiline = true, // Включение многострочного режима для возможности ввода одного символа
+                        MaxLength = 1, // Установка максимальной длины вводимого текста
+                        BackColor = backgroundColor, // Установка цвета фона ячейки
+                        Tag = new int[] { i, j } // Хранение координат ячейки в виде массива
+                    };
+                    cell.TextChanged += Cell_TextChanged; // Добавление обработчика события изменения текста
+                    sudokuCells[i, j] = cell; // Добавление ячейки в массив
+                    Controls.Add(cell); // Добавление ячейки на форму
+                }
+            }
+        }
+
+        // Метод для проверки конфликта значения в ячейке
+        private bool IsConflict(int row, int col, int value)
+        {
+            // Проверка строки
+            for (int i = 0; i < 9; i++)
+            {
+                if (i != col && sudokuCells[row, i].Text == value.ToString())
+                    return true;
+            }
+
+            // Проверка столбца
+            for (int i = 0; i < 9; i++)
+            {
+                if (i != row && sudokuCells[i, col].Text == value.ToString())
+                    return true;
+            }
+
+            // Проверка квадрата 3x3
+            int startRow = row - row % 3;
+            int startCol = col - col % 3;
+            for (int i = startRow; i < startRow + 3; i++)
+            {
+                for (int j = startCol; j < startCol + 3; j++)
+                {
+                    if ((i != row || j != col) && sudokuCells[i, j].Text == value.ToString())
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        // Обработчик события изменения текста в ячейке
+        private void Cell_TextChanged(object sender, EventArgs e)
+        {
+            TextBox cell = (TextBox)sender;
+
+            // Проверка на null
+            if (cell == null)
+                return;
+
+            int row = ((int[])cell.Tag)[0]; // Получение номера строки из тега ячейки
+            int col = ((int[])cell.Tag)[1]; // Получение номера столбца из тега ячейки
+
+            // Проверяем, содержит ли текст в ячейке непустые символы и не является ли первый символ управляющим символом
+            if (!string.IsNullOrEmpty(cell.Text) && !char.IsControl(cell.Text[0]))
+            {
+                if (!char.IsDigit(cell.Text[0]) || cell.Text[0] == '0')
+                {
+                    cell.Text = "";
+                    return;
+                }
+
+                int value = int.Parse(cell.Text);
+                if (IsConflict(row, col, value))
+                {
+                    // Окрашиваем конфликтные ячейки в красный
+                    HighlightConflicts(row, col, Color.Red);
+                }
+                else
+                {
+                    // Окрашиваем все ячейки в зеленый, если конфликтов нет
+                    HighlightConflicts(row, col, Color.Green);
+                }
+            }
+            else
+            {
+                // Если ячейка пустая, сбрасываем цвет на зеленый
+                HighlightConflicts(row, col, Color.Green);
+            }
+        }
+
+        // Метод для выделения конфликтных ячеек определенным цветом
+        private void HighlightConflicts(int row, int col, Color color)
+        {
+            // Окрашиваем всю строку
+            for (int i = 0; i < 9; i++)
+            {
+                sudokuCells[row, i].ForeColor = color;
+            }
+
+            // Окрашиваем всю колонку
+            for (int j = 0; j < 9; j++)
+            {
+                sudokuCells[j, col].ForeColor = color;
+            }
+
+            // Окрашиваем весь квадрат 3x3
+            int startRow = row - row % 3;
+            int startCol = col - col % 3;
+            for (int i = startRow; i < startRow + 3; i++)
+            {
+                for (int j = startCol; j < startCol + 3; j++)
+                {
+                    sudokuCells[i, j].ForeColor = color;
+                }
+            }
+        }
+
+        /*------------------------------ События нажатий на кнопки -------------------------*/
+
+        // Решение судоку
+        private static string[] completeSudoku =
+       {
+            "218375469",
+            "364289157",
+            "579461382",
+            "786932541",
+            "142758936",
+            "935146728",
+            "421893675",
+            "697524813",
+            "853617294"
+        };
+        private void BtnShowSolution_Click(object sender, EventArgs e)
+        {
+            // Заполнение судоку
+            for (int i = 0; i < sudokuCells.GetLength(0); i++)
+            {
+                for (int j = 0; j < sudokuCells.GetLength(1); j++)
+                {
+                    sudokuCells[i, j].Text = completeSudoku[i][j].ToString();
+                }
+            }
+        }
+
+        // Новая игра
+        private void BtnNewGame_Click(object sender, EventArgs e)
+        {
+            // Очистка текстовых полей
+            foreach (var cell in sudokuCells)
+            {
+                cell.Text = "";
+            }
+        }
+
+        // Наложение условий
+        private void BtnConditionImposed_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < sudokuCells.GetLength(0); i++)
+            {
+                for (int j = 0; j < sudokuCells.GetLength(1); j++)
+                {
+                    if (sudokuCells[i, j].ForeColor == Color.Red)
+                    {
+                        MessageBox.Show("Заполните все поля правильно!");
+                        return;
+                    }
+
+                    if (!string.IsNullOrEmpty(sudokuCells[i, j].Text))
+                    {
+                        sudokuCells[i, j].Enabled = false;
+                    }
+                }
+            }
+        }
+
+        // Разблокировка ячеек
+        private void BtnUnlockCells_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < sudokuCells.GetLength(0); i++)
+            {
+                for (int j = 0; j < sudokuCells.GetLength(1); j++)
+                {
+                    sudokuCells[i, j].Enabled = true;
+                }
+            }
+        }
+
+        // Сохранение состояния судоку в файл
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter("sudoku_save.txt"))
+                {
+                    // Запись текущего состояния судоку в файл
+                    for (int i = 0; i < sudokuCells.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < sudokuCells.GetLength(1); j++)
+                        {
+                            if (!string.IsNullOrEmpty(sudokuCells[i, j].Text))
+                            {
+                                writer.Write(sudokuCells[i, j].Text);
+                            }
+                            else
+                            {
+                                writer.Write("0"); // Записываем 0 для пустых ячеек
+                            }
+                        }
+                        writer.WriteLine(); // Переход на новую строку после каждой строки судоку
+                    }
+                }
+                MessageBox.Show("Состояние судоку сохранено успешно!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при сохранении состояния судоку: {ex.Message}");
+            }
+        }
+
+        // Загрузка состояния судоку из файла
+        private void BtnLoadSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader("sudoku_save.txt"))
+                {
+                    // Чтение состояния судоку из файла
+                    for (int i = 0; i < sudokuCells.GetLength(0); i++)
+                    {
+                        string line = reader.ReadLine();
+                        if (line != null && line.Length == sudokuCells.GetLength(1))
+                        {
+                            for (int j = 0; j < sudokuCells.GetLength(1); j++)
+                            {
+                                if (line[j] != '0')
+                                {
+                                    sudokuCells[i, j].Text = line[j].ToString();
+                                }
+                                else
+                                {
+                                    sudokuCells[i, j].Text = ""; // Очищаем текстовое поле, если значение ячейки равно 0
+                                }
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show("Состояние судоку загружено успешно!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке состояния судоку: {ex.Message}");
+            }
+        }
+    }
+}
