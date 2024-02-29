68830e708159b6bc97d188d414f1f9c001f01504
леонид коробков <korobkov-leo@mail.ru>
2024-02-29 19:31:57 +0300

    completed task 14

---
+++ Task_14/MainForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Task_14
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.inputSizeN = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.inputIntervalTimeMs = new System.Windows.Forms.NumericUpDown();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.inputSizeN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputIntervalTimeMs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(1312, 32);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(157, 71);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(1496, 32);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(157, 71);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // inputSizeN
+            // 
+            this.inputSizeN.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.inputSizeN.Location = new System.Drawing.Point(1312, 163);
+            this.inputSizeN.Margin = new System.Windows.Forms.Padding(6);
+            this.inputSizeN.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.inputSizeN.Name = "inputSizeN";
+            this.inputSizeN.Size = new System.Drawing.Size(240, 31);
+            this.inputSizeN.TabIndex = 12;
+            this.inputSizeN.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1307, 132);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Размер поля N";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1307, 242);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(246, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Интервал времени (мс)";
+            // 
+            // label
+            // 
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label.ForeColor = System.Drawing.Color.IndianRed;
+            this.label.Location = new System.Drawing.Point(1308, 330);
+            this.label.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(345, 96);
+            this.label.TabIndex = 9;
+            this.label.Text = "Чтобы применить настройки необходимо нажать на кнопку стоп, затем изменить настро" +
+    "йки и только потом нажать на старт\r\n";
+            // 
+            // inputIntervalTimeMs
+            // 
+            this.inputIntervalTimeMs.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.inputIntervalTimeMs.Location = new System.Drawing.Point(1312, 273);
+            this.inputIntervalTimeMs.Margin = new System.Windows.Forms.Padding(6);
+            this.inputIntervalTimeMs.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.inputIntervalTimeMs.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.inputIntervalTimeMs.Name = "inputIntervalTimeMs";
+            this.inputIntervalTimeMs.Size = new System.Drawing.Size(240, 31);
+            this.inputIntervalTimeMs.TabIndex = 13;
+            this.inputIntervalTimeMs.Value = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1694, 1000);
+            this.Controls.Add(this.inputIntervalTimeMs);
+            this.Controls.Add(this.inputSizeN);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.inputSizeN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputIntervalTimeMs)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.NumericUpDown inputSizeN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.NumericUpDown inputIntervalTimeMs;
+        private System.Windows.Forms.Timer timer;
+    }
+}
---
+++ Task_14/MainForm.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Task_14
+{
+    public partial class MainForm : Form
+    {
+        // Переменные для хранения размера сетки, интервала обновления и состояний заражения клеток
+        private int gridSize;
+        private int interval;
+        private int[,] infectionTicks;
+        private CellPanel[,] gridPanels;
+        private bool isSimulationRunning = false;
+
+        // Количество тиков заражения и иммунитета
+        private int amountInfectionTicks = 6;
+        private int amountImmunTicks = 4;
+
+        private Random random = new Random();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // Обновление настроек сетки и таймера
+        private void UpdateSettings()
+        {
+            interval = (int)inputIntervalTimeMs.Value;
+            gridSize = (int)inputSizeN.Value;
+
+            timer.Interval = interval;
+        }
+
+        // Включение/выключение элементов управления настроками симуляции
+        private void ToggleSettingsEnabled(bool isEnabled)
+        {
+            inputSizeN.Enabled = isEnabled;
+            inputIntervalTimeMs.Enabled = isEnabled;
+        }
+
+        // Включение/выключение элементов управления симуляцией
+        private void ToggleSimulationControlsEnabled(bool isRunning)
+        {
+            btnStart.Enabled = !isRunning;
+            btnStop.Enabled = isRunning;
+            ToggleSettingsEnabled(!isRunning);
+        }
+
+        // Начало симуляции
+        private void StartSimulation()
+        {
+            isSimulationRunning = true;
+            UpdateSettings();
+
+            // Вычисление размера каждой ячейки сетки
+            int panelSize = 100 / (gridSize / 3);
+
+            // Инициализация массивов для хранения количества тиков времени и состояний клеток
+            infectionTicks = new int[gridSize, gridSize];
+            gridPanels = new CellPanel[gridSize, gridSize];
+
+            // Создание ячеек сетки и их добавление на форму
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    infectionTicks[i, j] = 0;
+                    gridPanels[i, j] = new CellPanel()
+                    {
+                        Size = new Size(panelSize, panelSize),
+                        Location = new Point((5 + panelSize) * i, (5 + panelSize) * j)
+                    };
+                    Controls.Add(gridPanels[i, j]);
+                }
+            }
+
+            // Установка исходной зараженной клетки
+            gridPanels[gridSize / 2, gridSize / 2].State = CellState.Infected;
+            timer.Start();
+
+            // Включение/выключение элементов управления
+            ToggleSimulationControlsEnabled(true);
+        }
+
+        // Остановка симуляции
+        private void StopSimulation()
+        {
+            isSimulationRunning = false;
+            // Удаление всех ячеек с формы
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    Controls.Remove(gridPanels[i, j]);
+                }
+            }
+            timer.Stop();
+
+            // Включение/выключение элементов управления
+            ToggleSimulationControlsEnabled(false);
+        }
+
+        // Обработчик нажатия кнопки "Start"
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!isSimulationRunning)
+            {
+                StartSimulation();
+            }
+        }
+
+        // Обработчик нажатия кнопки "Stop"
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (isSimulationRunning)
+            {
+                StopSimulation();
+            }
+        }
+
+        // Обработчик события таймера, отвечающий за процесс симуляции
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // Обход всех клеток сетки
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    // Увеличение счетчика заражения для всех нездоровых клеток
+                    if (gridPanels[i, j].State != CellState.Healthy)
+                        infectionTicks[i, j]++;
+
+                    // Попытка заражения соседних клеток
+                    int randomValue = random.Next(2);
+                    if (gridPanels[i, j].State == CellState.Infected)
+                    {
+                        if (randomValue == 1)
+                            TryInfect(i, j);
+                    }
+
+                    // Проверка состояния зараженности клетки
+                    CheckInfectionTicks(i, j);
+                }
+            }
+        }
+
+        // Попытка заражения соседних клеток
+        private void TryInfect(int x, int y)
+        {
+            int[,] neighbors = { { x + 1, y }, { x - 1, y }, { x, y - 1 }, { x, y + 1 } };
+
+            for (int i = 0; i < neighbors.GetLength(0); i++)
+            {
+                int randomIndex = random.Next(4);
+                int newX = neighbors[randomIndex, 0];
+                int newY = neighbors[randomIndex, 1];
+
+                if (IsValidCoordinate(newX, newY) && gridPanels[newX, newY].State == CellState.Healthy)
+                {
+                    gridPanels[newX, newY].State = CellState.Infected;
+                }
+            }
+        }
+
+        // Проверка допустимости координат клетки
+        private bool IsValidCoordinate(int x, int y)
+        {
+            return (x >= 0 && y >= 0 && x < gridSize && y < gridSize);
+        }
+
+        // Обработка счетчика заражения для клетки
+        private void CheckInfectionTicks(int x, int y)
+        {
+            CellState cellState = gridPanels[x, y].State;
+
+            // Если клетка заражена и прошло достаточно времени, изменить состояние на иммунное
+            if (cellState == CellState.Infected && infectionTicks[x, y] >= amountInfectionTicks)
+            {
+                gridPanels[x, y].State = CellState.Immune;
+                infectionTicks[x, y] = 0;
+            }
+            // Если клетка иммунна и прошло достаточно времени, изменить состояние на здоровое
+            else if (cellState == CellState.Immune && infectionTicks[x, y] >= amountImmunTicks)
+            {
+                gridPanels[x, y].State = CellState.Healthy;
+                infectionTicks[x, y] = 0;
+            }
+        }
+    }
+
+    // Перечисление для состояния клетки
+    public enum CellState
+    {
+        Healthy,
+        Infected,
+        Immune
+    }
+
+    // Пользовательский класс для панели клетки
+    public class CellPanel : Panel
+    {
+        // Свойство для хранения состояния клетки
+        private CellState state;
+
+        // Свойство для доступа к состоянию клетки
+        public CellState State
+        {
+            get { return state; }
+            set
+            {
+                state = value;
+                UpdateColor(); // Обновление цвета панели при изменении состояния
+            }
+        }
+
+        // Конструктор класса, устанавливает исходное состояние и цвет панели
+        public CellPanel()
+        {
+            state = CellState.Healthy;
+            UpdateColor();
+        }
+
+        // Обновление цвета панели в зависимости от состояния клетки
+        private void UpdateColor()
+        {
+            switch (state)
+            {
+                case CellState.Healthy:
+                    this.BackColor = Color.White;
+                    break;
+                case CellState.Infected:
+                    this.BackColor = Color.Red;
+                    break;
+                case CellState.Immune:
+                    this.BackColor = Color.Green;
+                    break;
+            }
+        }
+    }
+}
